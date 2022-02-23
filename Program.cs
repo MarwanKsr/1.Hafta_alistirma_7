936f8d3233be504877fc1f889c7a59888c171075
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-23 11:50:04 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace alistirma_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cumleyi giriniz: ");
+            string cumle = Console.ReadLine();
+            int sayi = 0;
+            for (int i=0; i<cumle.Length; i++)
+            {
+                if (cumle[i] != ' ')
+                {
+                    sayi++;
+                }
+            }
+            Console.WriteLine("Cumledeki harf sayisi: " + sayi);
+        }
+    }
+}
